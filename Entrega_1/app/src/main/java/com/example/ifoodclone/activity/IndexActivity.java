57c8585b1666a91709d80c64af278f08d6094e00
24026811 <24026811@fecap.br>
2025-09-16 22:09:31 -0300

    tela index construída

---
+++ Entrega_1/app/src/main/java/com/example/ifoodclone/activity/IndexActivity.java
@@ -0,0 +1,38 @@
+package com.example.ifoodclone.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ifoodclone.R;
+import com.example.ifoodclone.helper.FirebaseConfiguration;
+import com.example.ifoodclone.helper.FirebaseUserConfiguration;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+
+
+    }
+}
