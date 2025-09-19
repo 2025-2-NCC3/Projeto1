83768d11de9c5dae3f01cccce00bea527d16776c
24026811 <24026811@fecap.br>
2025-09-18 22:28:12 -0300

    layout tela de inicio atualizado + interacoes de login cadastro

--- Entrega_1/app/src/main/java/com/example/ifoodclone/activity/IndexActivity.java
+++ Entrega_1/app/src/main/java/com/example/ifoodclone/activity/IndexActivity.java
@@ -1,37 +1,35 @@
 package com.example.ifoodclone.activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.ifoodclone.R;
-import com.example.ifoodclone.helper.FirebaseConfiguration;
-import com.example.ifoodclone.helper.FirebaseUserConfiguration;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.auth.FirebaseUser;
 
 public class IndexActivity extends AppCompatActivity {
+
+    private Button buttonEnter, buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
+        buttonEnter = findViewById(R.id.buttonEnter); // ou R.id.buttonEnter, se mudar no XML
+
+        buttonEnter.setOnClickListener(v -> {
+            Intent intent = new Intent(IndexActivity.this, AuthenticationActivity.class);
+            startActivity(intent);
+        });
+
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(IndexActivity.this, AuthenticationActivity.class);
+            intent.putExtra("MODE", "REGISTER"); // Passa a informação de cadastro
+            startActivity(intent);
+        });
 
 
     }
